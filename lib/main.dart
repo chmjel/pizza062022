e6a4eeedb1edbca4f05ba057ad0188e3aa5f4829
Ola Chmielewska <a.chmielewska01@gmail.com>
2022-07-09 09:43:17 +0200

    Move MyApp to app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:pizza062022/app/features/home/home_page.dart';
-import 'package:pizza062022/app/features/home/login/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:pizza062022/app/app.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -13,37 +11,3 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RootPage(),
-    );
-  }
-}
-
-class RootPage extends StatelessWidget {
-  const RootPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          final user = snapshot.data;
-          if (user == null) {
-            return LoginPage();
-          }
-          return HomePage(user: user);
-        });
-  }
-}
